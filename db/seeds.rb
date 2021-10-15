da2352220691e1ff49445423c63d3019e85172c6
3429 <sana.2980mizuiro@au.com>
2021-10-15 11:13:22 +0900

    テストデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(account: 's20191424', password: 2766, schoolyear: 3, username: '錦織太成', nickname: 'じゃがいも')
 User.create(account: 's20191415', password: 3504, schoolyear: 3, username: '佐藤祐介', nickname: '主将')
+
+CSV.foreach('db/course2.csv') do |row|
+  Course.create(:name => row[0], :kinds => row[1], :year => row[2], :contents => row[3], :remarks => row[4])
+end
+
+CSV.foreach('db/middle.csv') do |row|
+  Middle.create(:course_id => row[0], :kubun_id => row[1])
+end
+
+CSV.foreach('db/kubun2.csv') do |row|
+  Kubun.create(:code => row[0], :name => row[1])
+end
